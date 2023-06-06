213940c2f6ff6103ee4b761fdd44c3fb1552bb74
Dudu <aeiou7352@gmail.com>
2023-06-06 20:58:57 +0200

    Removed unecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import Sidebar from "./sidebar/Sidebar";
 import Planner from "./planner/Planner";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
   return (
     <div className="App">
