a2154e78db041257aab7c6bf21f8a19cd9749608
Dudu7352 <aeiou7352@gmail.com>
2023-06-24 08:50:50 +0200

    Added Suspense inside the Sidebar

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,44 +1,43 @@
 import { invoke } from "@tauri-apps/api/tauri";
-import { useState, useEffect, Dispatch } from "react";
+import { useState, useEffect, Dispatch, Suspense } from "react";
 import "./Sidebar.css";
 import CalendarBar from "./calendar_bar/CalendarBar";
 import WeekSelector from "./week_selector/WeekSelector";
 import { MonthDetails, YearDetails } from "../utils/interfaces";
 
 interface SidebarProps {
-  userYear: number,
-  currentYear: number,
-  setUserYear: (year: number) => void,
-  setWeek: React.Dispatch<React.SetStateAction<number>>,
+  userYear: number;
+  currentYear: number;
+  setUserYear: (year: number) => void;
+  setWeek: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export default function Sidebar(props: SidebarProps) {
   let [yearDetails, setYearDetails] = useState<YearDetails>({
     year: 0,
     month_details_list: Array<MonthDetails>(12),
-    isLeap: false
+    isLeap: false,
   });
 
   useEffect(() => {
-    invoke("get_year_details", { year: props.userYear}).then(yearDetailsMsg => {
-      setYearDetails(yearDetailsMsg as YearDetails);
-    });
+    invoke("get_year_details", { year: props.userYear }).then(
+      (yearDetailsMsg) => {
+        setYearDetails(yearDetailsMsg as YearDetails);
+      }
+    );
   }, [props.userYear]);
 
   return (
     <div className="Sidebar">
-      <CalendarBar 
-        year={
-          props.currentYear
-        }
-        setYear={props.setUserYear}
-      />
-      <WeekSelector
-        monthDetails={
-          yearDetails.month_details_list
-        }
-        setWeek={props.setWeek}
-      />
+      <Suspense fallback={(
+        <div>Loading...</div>
+      )}>
+        <CalendarBar year={props.currentYear} setYear={props.setUserYear} />
+        <WeekSelector
+          monthDetails={yearDetails.month_details_list}
+          setWeek={props.setWeek}
+        />
+      </Suspense>
     </div>
   );
 }
