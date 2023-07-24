9fd1687b5d1ed19f241bd0b4a1a7f3441bba0510
Dudu7352 <aeiou7352@gmail.com>
2023-07-24 11:09:21 +0200

    Updated shade functions in typescript

--- src/utils/functions/shades.ts
+++ src/utils/functions/shades.ts
@@ -1,13 +1,16 @@
-import { SHADE_DOWN } from "../consts";
+import { SHADE_DOWN, SHADE_UP } from "../consts";
 import { clamp } from "./clamp";
 
-export function shadeDown(color_str: string): string {
-  let color = parseInt(color_str.substring(1), 16);
+export function shade(color_str: string, shade: number): string {
+  const color = parseInt(color_str.substring(1), 16);
   let r = color >> 16;
   let g = color >> 8 % 256;
   let b = color % 256;
-  r = clamp(r * SHADE_DOWN, 0, 255);
-  g = clamp(g * SHADE_DOWN, 0, 255);
-  b = clamp(b * SHADE_DOWN, 0, 255);
+  r = clamp(r * shade, 0, 255);
+  g = clamp(g * shade, 0, 255);
+  b = clamp(b * shade, 0, 255);
   return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
+
+export let shadeDown = (color_str: string) => shade(color_str, SHADE_DOWN);
+export let shadeUp = (color_str: string) => shade(color_str, SHADE_UP);
