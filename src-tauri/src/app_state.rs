98edea2c8da3db26bbf8fdbd2d4b364c6fb8ff46
Dudu7352 <aeiou7352@gmail.com>
2023-08-30 22:56:00 +0200

    Unified tabs in rust

--- src-tauri/src/app_state.rs
+++ src-tauri/src/app_state.rs
@@ -1,115 +1,113 @@
-use std::{ collections::HashMap, ops::Index, path::PathBuf, fs::create_dir_all };
+use std::{collections::HashMap, fs::create_dir_all, ops::Index, path::PathBuf};
 
-use chrono::{ Duration, NaiveDate };
+use chrono::{Duration, NaiveDate};
 use serde::Serialize;
 
 use crate::{
-  utils::{events_collide, get_data_path},
-  event_structures::event_type::EventType,
-  file_manager::FileManager,
-  color_structures::color::Color,
+    color_structures::color::Color,
+    event_structures::event_type::EventType,
+    file_manager::FileManager,
+    utils::{events_collide, get_data_path},
 };
 
 #[derive(Serialize)]
 pub struct AppState {
-  pub event_list: HashMap<NaiveDate, Vec<EventType>>,
-  pub color_list: Vec<Color>,
-  events_file_path: PathBuf,
-  colors_file_path: PathBuf,
+    pub event_list: HashMap<NaiveDate, Vec<EventType>>,
+    pub color_list: Vec<Color>,
+    events_file_path: PathBuf,
+    colors_file_path: PathBuf,
 }
 
 impl AppState {
-  pub fn new() -> Self {
-    let data_path = get_data_path();
-    let _ = create_dir_all(&data_path);
-    
-    let mut events_file_path = data_path.clone();
-    let mut colors_file_path = data_path.clone();
+    pub fn new() -> Self {
+        let data_path = get_data_path();
+        let _ = create_dir_all(&data_path);
 
-    events_file_path.push("plans-app");
-    events_file_path.set_file_name("events");
-    events_file_path.set_extension("json");
-        
-    colors_file_path.push("plans-app");
-    colors_file_path.set_file_name("colors");
-    colors_file_path.set_extension("json");
+        let mut events_file_path = data_path.clone();
+        let mut colors_file_path = data_path.clone();
 
-    
-    let event_list = FileManager::load_data::<HashMap<NaiveDate, Vec<EventType>>>(
-      &events_file_path
-    ).unwrap_or(HashMap::new());
-    let color_list = match FileManager::load_data::<Vec<Color>>(&colors_file_path) {
-      Ok(file_data) => file_data,
-      Err(_) => {
-        let new_data: Vec<Color> = vec![
-          Color::new(32, 191, 85),
-          Color::new(11, 79, 108),
-          Color::new(254, 93, 38)
-        ];
-        FileManager::save_data(&colors_file_path, &new_data);
-        new_data
-      }
-    };
+        events_file_path.push("plans-app");
+        events_file_path.set_file_name("events");
+        events_file_path.set_extension("json");
 
-    Self {
-      event_list,
-      color_list,
-      events_file_path,
-      colors_file_path,
-    }
-  }
+        colors_file_path.push("plans-app");
+        colors_file_path.set_file_name("colors");
+        colors_file_path.set_extension("json");
 
-  pub fn add_event(&mut self, e: EventType) -> Result<(), ()> {
-    let date_time = e.get_date_time();
-    let day_key = date_time.date();
-
-    if let EventType::EVENT(new_event) = &e {
-      let new_event_end =
-        new_event.date_time + Duration::minutes(new_event.duration_minutes as i64);
-
-      if new_event.date_time.date() != new_event_end.date() {
-        return Err(());
-      }
-    }
-
-    match self.event_list.get_mut(&day_key) {
-      Some(day_list) => {
-        for event in day_list.iter() {
-          if events_collide(event, &e) {
-            return Err(());
-          }
-        }
-        day_list.push(e);
-      }
-      None => {
-        self.event_list.insert(day_key, vec![e]);
-      }
-    }
-    FileManager::save_data::<HashMap<NaiveDate, Vec<EventType>>>(
-      &self.events_file_path,
-      &self.event_list
-    );
-    Ok(())
-  }
-
-  pub fn delete_event(&mut self, new_event: EventType) -> Result<(), ()> {
-    match self.event_list.get_mut(&new_event.get_date_time().date()) {
-      Some(day_list) =>
-        match day_list.iter().position(|r| r == &new_event) {
-          Some(index) => {
-            for i in index + 1..day_list.len() {
-              day_list[i - 1] = day_list.index(i).clone();
+        let event_list =
+            FileManager::load_data::<HashMap<NaiveDate, Vec<EventType>>>(&events_file_path)
+                .unwrap_or(HashMap::new());
+        let color_list = match FileManager::load_data::<Vec<Color>>(&colors_file_path) {
+            Ok(file_data) => file_data,
+            Err(_) => {
+                let new_data: Vec<Color> = vec![
+                    Color::new(32, 191, 85),
+                    Color::new(11, 79, 108),
+                    Color::new(254, 93, 38),
+                ];
+                FileManager::save_data(&colors_file_path, &new_data);
+                new_data
             }
-            day_list.pop();
-            FileManager::save_data::<HashMap<NaiveDate, Vec<EventType>>>(
-              &self.events_file_path,
-              &self.event_list
-            );
-            Ok(())
-          }
-          None => Err(()),
+        };
+
+        Self {
+            event_list,
+            color_list,
+            events_file_path,
+            colors_file_path,
         }
-      None => Err(()),
     }
-  }
+
+    pub fn add_event(&mut self, e: EventType) -> Result<(), ()> {
+        let date_time = e.get_date_time();
+        let day_key = date_time.date();
+
+        if let EventType::EVENT(new_event) = &e {
+            let new_event_end =
+                new_event.date_time + Duration::minutes(new_event.duration_minutes as i64);
+
+            if new_event.date_time.date() != new_event_end.date() {
+                return Err(());
+            }
+        }
+
+        match self.event_list.get_mut(&day_key) {
+            Some(day_list) => {
+                for event in day_list.iter() {
+                    if events_collide(event, &e) {
+                        return Err(());
+                    }
+                }
+                day_list.push(e);
+            }
+            None => {
+                self.event_list.insert(day_key, vec![e]);
+            }
+        }
+        FileManager::save_data::<HashMap<NaiveDate, Vec<EventType>>>(
+            &self.events_file_path,
+            &self.event_list,
+        );
+        Ok(())
+    }
+
+    pub fn delete_event(&mut self, new_event: EventType) -> Result<(), ()> {
+        match self.event_list.get_mut(&new_event.get_date_time().date()) {
+            Some(day_list) => match day_list.iter().position(|r| r == &new_event) {
+                Some(index) => {
+                    for i in index + 1..day_list.len() {
+                        day_list[i - 1] = day_list.index(i).clone();
+                    }
+                    day_list.pop();
+                    FileManager::save_data::<HashMap<NaiveDate, Vec<EventType>>>(
+                        &self.events_file_path,
+                        &self.event_list,
+                    );
+                    Ok(())
+                }
+                None => Err(()),
+            },
+            None => Err(()),
+        }
+    }
 }
--- src-tauri/src/conn/conn.rs
+++ src-tauri/src/conn/conn.rs
@@ -1,35 +1,42 @@
-use sqlx::{pool::Pool, SqlitePool, Sqlite, sqlite::SqlitePoolOptions};
+use sqlx::{pool::Pool, sqlite::SqlitePoolOptions, Sqlite, SqlitePool};
+
+use crate::event_structures::event_details::EventDetails;
 
 pub struct Conn {
-  pool: Pool<Sqlite>
+    pool: Pool<Sqlite>,
 }
 
 impl Conn {
-  pub async fn new(url: &str) -> Self {
-      let pool_options = SqlitePoolOptions::new()
-        .max_connections(1);
-      Self {
-          pool: pool_options.connect(url).await.unwrap() //TODO: Handle result
-      }
-  }
+    pub async fn new(url: &str) -> Self {
+        let pool_options = SqlitePoolOptions::new().max_connections(1);
+        Self {
+            pool: pool_options.connect(url).await.unwrap(), //TODO: Handle result
+        }
+    }
 
-  pub async fn setup(&self) -> Result<(), ()> {
-      let event_table_result = sqlx::query("CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY NOT NULL AUTOINCEREMENT, date_time INTEGER NOT NULL, name TEXT NOT NULL, duration_minutes INTEGET NOT NULL, color TEXT)")
-          .execute(&self.pool).await;
+    pub async fn setup(&self) -> Result<(), ()> {
+        let event_table_result = sqlx
+      ::query(
+        "CREATE TABLE IF NOT EXISTS event (id INTEGER PRIMARY KEY NOT NULL AUTOINCEREMENT, date_time INTEGER NOT NULL, name TEXT NOT NULL, duration_minutes INTEGET NOT NULL, color TEXT)"
+      )
+      .execute(&self.pool).await;
 
-      if let Err(err) = event_table_result {
-          println!("Could not create event table. Error: {:?}", err);
-          return Err(());
-      }
+        if let Err(err) = event_table_result {
+            println!("Could not create event table. Error: {:?}", err);
+            return Err(());
+        }
 
-      let deadline_table_result = sqlx::query("CREATE TABLE IF NOT EXISTS deadlines (id INTEGER PRIMARY KEY NOT NULL AUTOINCEREMENT, date_time INTEGER NOT NULL, name TEXT NOT NULL, color TEXT)")
-          .execute(&self.pool).await;
+        let deadline_table_result = sqlx
+      ::query(
+        "CREATE TABLE IF NOT EXISTS deadline (id INTEGER PRIMARY KEY NOT NULL AUTOINCEREMENT, date_time INTEGER NOT NULL, name TEXT NOT NULL, color TEXT)"
+      )
+      .execute(&self.pool).await;
 
-      if let Err(err) = deadline_table_result {
-          println!("Could not create deadline table. Error: {:?}", err);
-          return Err(());
-      }
+        if let Err(err) = deadline_table_result {
+            println!("Could not create deadline table. Error: {:?}", err);
+            return Err(());
+        }
 
-      Ok(())
-  }
+        Ok(())
+    }
 }
