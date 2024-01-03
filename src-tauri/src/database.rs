58530f35784f775ad9740302ffa1a769b77a6170
Dudu <aeiou7352@gmail.com>
2024-01-03 18:52:45 +0100

    feat: implement get_entries method in PlansDbConn

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,7 +1,9 @@
 use chrono::NaiveDateTime;
-use diesel::{prelude::SqliteConnection, Connection};
+use diesel::RunQueryDsl;
+use diesel::{prelude::SqliteConnection, Connection, QueryDsl, ExpressionMethods, SelectableHelper};
 
-use crate::{utils::get_database_path, event_structures::entry::Entry};
+use crate::event_structures::calendar_deadline::CalendarDeadline;
+use crate::{utils::get_database_path, event_structures::{entry::Entry, calendar_event::CalendarEvent}, schema};
 
 struct PlansDbConn {
     conn: SqliteConnection
@@ -15,8 +17,21 @@ impl PlansDbConn {
         }
     }
 
-    pub fn get_entries(&self, start: NaiveDateTime, end: NaiveDateTime) -> Vec<Entry> {
-        todo!()
+    pub fn get_entries(&mut self, start: NaiveDateTime, end: NaiveDateTime) -> Vec<Entry> {
+        use schema::calendar_event::dsl::*;
+        use schema::calendar_deadline::dsl::*;
+        let mut entries: Vec<Entry> = Vec::new();
+        entries.extend(calendar_event
+            .filter(schema::calendar_event::date_start.ge(start))
+            .filter(schema::calendar_event::date_end.le(end))
+            .select(CalendarEvent::as_select())
+            .load::<CalendarEvent>(&mut self.conn).map_or(vec![], |v| v.into_iter().map(|e| Entry::Event(e)).collect()));
+        entries.extend(calendar_deadline
+            .filter(schema::calendar_deadline::date_until.ge(start))
+            .filter(schema::calendar_deadline::date_until.le(end))
+            .select(CalendarDeadline::as_select())
+            .load::<CalendarDeadline>(&mut self.conn).map_or(vec![], |v| v.into_iter().map(|e| Entry::Deadline(e)).collect()));
+        entries
     }
 
     pub fn insert_entry(&self, calendar_entry: Entry) {
