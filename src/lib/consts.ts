7453c60482bc0183f7b99a7aa37ec187787eeb9f
Dudu7352 <aeiou7352@gmail.com>
2024-05-19 21:50:22 +0200

    remove unnecessary consts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,6 +1,3 @@
-import { Time } from "./classes";
-import { IEventDetails, Entry } from "./interfaces";
-
 export const MONTHS: string[] = [
   "January",
   "February",
@@ -37,18 +34,6 @@ export const YEAR_DAYS = 365;
 export const LEAP_YEAR_DAYS = 366;
 
 export const DEFAULT_DATE: Date = new Date("2000-01-01");
-export const DEFAULT_TIME: Time = new Time("00:00");
-export const DEFAULT_EVENT: IEventDetails = {
-  calendarEntryId: "",
-  dateStart: Math.floor(DEFAULT_DATE.getTime() / 1000),
-  dateEnd: Math.floor(DEFAULT_DATE.getTime() / 1000 + 1),
-  color: "#ff0000",
-  eventName: "",
-};
-export const DEFAULT_EVENT_TYPE: Entry = {
-  Event: undefined,
-  Deadline: undefined,
-};
 
 export const COLORS: string[] = [
   "#20bf55",
