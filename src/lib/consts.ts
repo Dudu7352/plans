97800cd76b697c86a43efc9e122e32431bafda65
Dudu7352 <aeiou7352@gmail.com>
2024-05-19 21:51:47 +0200

    remove unnecessary consts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -23,23 +23,5 @@ export const WEEKDAYS: string[] = [
   "Saturday",
 ];
 
-export enum Prompt {
-  NONE,
-  ADD,
-  EDIT,
-}
-
-export const YEAR_WEEKS = 53;
-export const YEAR_DAYS = 365;
-export const LEAP_YEAR_DAYS = 366;
-
-export const DEFAULT_DATE: Date = new Date("2000-01-01");
-
-export const COLORS: string[] = [
-  "#20bf55",
-  "#0b4f6c",
-  "#fe5d26"
-];
-
 export const SHADE_DOWN = 0.9;
 export const SHADE_UP = 1.1;
